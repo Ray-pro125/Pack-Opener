603fb35cffc0269b763c3f9c2e774944f0e911da
Ray-pro125 <sacha.lavalvanhamme@gmail.com>
2026-01-08 18:50:39 +0100

    Update script.js

--- script.js
+++ script.js
@@ -134,20 +134,22 @@ function openPack(){
     div.className=`card rarity-${c.rarity.replace(/\s+/g,'-')}`;
     div.innerHTML=`<img src="${c.image}" alt="${c.name}">`;
 
-   // Only last 3 cards are click-to-reveal 
-    if(i < pulls.length - 3){ 
-      setTimeout(()=>div.classList.add("show"), i*350); 
-    } else { 
-      div.classList.add("last-three-hidden"); 
-      div.addEventListener("click", function reveal(){ 
-        div.classList.add("show"); 
-        div.classList.remove("last-three-hidden"); 
-        div.removeEventListener("click", reveal); 
-      }); 
-    } 
-    
+  if (i < pulls.length - 3) {
+  setTimeout(() => div.classList.add("show"), i * 350);
+  packDiv.appendChild(div);
+} else {
+  // Delay adding glow until all previous cards are shown
+  setTimeout(() => {
+    div.classList.add("last-three-hidden"); // glow appears but image hidden
     packDiv.appendChild(div);
-  });
+
+    // Click-to-reveal
+    div.addEventListener("click", function reveal() {
+      div.classList.add("show");
+      div.classList.remove("last-three-hidden");
+      div.removeEventListener("click", reveal);
+    });
+  }, (pulls.length - 3) * 350);
 }
 
 /* ---------------- START SCREEN ---------------- */
